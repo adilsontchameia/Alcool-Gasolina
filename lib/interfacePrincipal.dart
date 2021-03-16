e1b13cbacbceec0f60aa1b3b1f57a5022910c133
adilsonchameia <adilsonkchameia@gmail.com>
2021-03-16 21:40:33 +0100

    Aprimorando a interface.

--- lib/interfacePrincipal.dart
+++ lib/interfacePrincipal.dart
@@ -11,7 +11,47 @@ class _InterfaceState extends State<Interface> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Alcool ou Gasolina"),
-        backgroundColor: Colors.cyan,
+        backgroundColor: Colors.blue,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(32),
+      //Coluna
+      child: Column(
+        children: [
+          //Padding Logo
+          Padding(
+            padding: EdgeInsets.only(bottom: 32),
+            child: Image.asset("images/logo.png"),
+          ),
+          //Segundo Texto
+          Padding(
+            padding: EdgeInsets.only(bottom: 10),
+            child: Text("Saiba a melhor opcao para o seu carro.",
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold
+            ),),
+          ),
+          //Primeiro Campo Texto
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: "Preco do Alcool, exe: 1.59"
+            ),
+          ),
+          //Segundo Campo Texto
+          TextField(
+            
+          ),
+          //Botado
+          Padding(
+            padding: EdgeInsets.only(top: 10),
+            child: RaisedButton( 
+              child: Text("Calcular"),
+              onPressed: (){}),
+          ),
+        ],
+      ), 
       ),
     );
   }
